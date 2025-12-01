bb558ad5cf07105cdaa153c4fe450dc36520b133
Darckstar55th <j4352017@gmail.com>
2025-12-01 16:05:21 -0500

    funcion final añadida

--- ej7.cpp
+++ ej7.cpp
@@ -9,7 +9,7 @@ using namespace std;
     double menor = 1000;
     double aux;
      for (int i = 0; i < 7; i++){
-      cout<<"Introduce la temperatura del dia: "<<i + 1<<endl;
+      cout<<"Introduce la temperatura del dia en grados celcius: "<<i + 1<<endl;
         cin>>temp[i];
      }
      for (int i = 0; i < 7; i++){
@@ -29,22 +29,27 @@ using namespace std;
 
      for (int i = 0; i < 7; i++){
       gradosFarenheight[i] = temp[i] * 1.8 + 32.0 ;
-     }
-      
-      cout<<"\n========================Temperaturas en grados celcius============================="<<endl;
+     } 
+      cout<<"\n========================Temperaturas en grados celcius=============================\n"<<endl;
      for (int i = 0; i < 7; i++){
       cout<<temp[i]<<" ";
      }
-     cout<<"\n========================Temperaturas en grados farenheit============================="<<endl;
+     cout<<"\n========================Temperaturas en grados farenheit=============================\n"<<endl;
      
      for (int i = 0; i < 7; i++){
        cout<<gradosFarenheight[i]<<" ";
      }
-      cout<<"\n==========Mayor, menor y promedio de temperaturas==============="<<endl;
-    
+     cout<<"\n==========Mayor, menor y promedio de temperaturas===============\n"<<endl;
+     cout.precision(2);
      cout<<"La mayor temperatura registrada fue de: "<<mayor<<" grados celcius"<<endl;
      cout<<"La menor temperatura registrada fue de: "<<menor<<" grados celcius"<<endl;
-     cout<<"El promedio de temperaturas en la semana fue de: "<<promedio<<" grados celcius";
-      
+     cout<<"El promedio de temperaturas en la semana fue de: "<<promedio<<" grados celcius"<<endl;
+     cout<<"Los dias con temperaturas por encima del promedio son: "<<endl;
+      for (int i = 0; i < 7; i++){
+          if (temp[i] > promedio){
+            cout<<"\nDia "<< i+1 << ": "<<temp[i]<<" grados celcius";
+          }
+          
+     }
     return 0;
   }
